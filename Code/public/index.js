1bb2e4779dad494328a83098d1eae43b4f6aafb8
dany123000 <83404634+dany123000@users.noreply.github.com>
2022-03-22 15:54:53 +0100

    merge api.js and index.js

--- Code/public/index.js
+++ Code/public/index.js
@@ -1,5 +1,4 @@
-//const api = require("./api.js")
-
+'use strict';
 
 const btn_trajet = document.querySelector('#trajet-btn')
 //const btn_gare = document.querySelector('#gare-btn')
@@ -46,6 +45,7 @@ btn_send.addEventListener('click', async () =>{
     if(btn_send.value == 'trajet'){
         const gare_dep = document.querySelector('#dep_station').value
         const gare_arr = document.querySelector('#arr_station').value
+        console.log(gare_dep,gare_arr)
         // Find stations UIC in the db with SPARQL
         uic_dep = "123456789"
         uic_arr = "123456789"
@@ -61,3 +61,79 @@ btn_send.addEventListener('click', async () =>{
 document.addEventListener('DOMContentLoaded', ()=>{
     renderForm('trajet')
 })
+
+
+
+// Requests for API
+
+
+const sncf_api_key = "6430b7e5-f72b-46a3-85f2-6920e725b010"
+const sncf_weather_key = "6430b7e5-f72b-46a3-85f2-6920e725b010"
+const options_fetch = {
+    headers:{
+    "Authorization": "6430b7e5-f72b-46a3-85f2-6920e725b010"
+  }
+}
+
+
+const findTrainJourney = async id => {
+    let today = new Date()
+    let month = today.getMonth()+1
+    let day = today.getDate();
+    let date = today.getFullYear()+'-'+(month<10?'0'+month : month)+'-'+ (day < 10 ? '0'+day : day);
+    console.log("Date : ", date)
+    try {
+        let url = `https://api.sncf.com/v1/coverage/sncf/vehicle_journeys/vehicle_journey:SNCF:${date}:${id}:1187:LongDistanceTrain`
+        var response = await fetch(url, options_fetch)
+        var trains = await response.json();
+        if(trains.hasOwnProperty("error")){
+            response = await fetch(`https://api.sncf.com/v1/coverage/sncf/vehicle_journeys/vehicle_journey:SNCF:${date}:${id}:1187:Train`, options_fetch);
+            trains = await response.json();
+            if(trains.hasOwnProperty("error")){
+                trains = {};
+            }
+        }
+        console.log(trains)
+        return trains;
+    }catch (err) {
+        console.log("Can't find the train", err)
+        return {};
+    }
+}
+
+const findTrainStation = async (departure, arrival) => {
+    try {
+        var response = await fetch(`https://api.sncf.com/v1/coverage/sncf/stop_points/stop_point:SNCF:${departure}:Train/departures`, options_fetch);
+        var trains = await response.json();
+        if(trains.hasOwnProperty("error")){
+            trains = {};
+        }
+        else{
+            var res = trains["departures"].filter(dep=>dep["route"]["direction"]["stop_area"]["codes"][1]["value"] === arrival)
+            res = res[0]["links"].filter(lk=>lk["type"] === "vehicle_journey");
+            res = res[0]["id"];
+            res = res.split(':');
+            return findTrainJourney(res[3]);
+        }
+        return trains;
+    }catch (err) {
+        console.log("Can't find the train", err)
+        return {};
+    }
+}
+
+const findweather = async (latitude, longitude) => {
+    try {
+        const response = await fetch(
+            `https://www.infoclimat.fr/public-api/gfs/json?_ll=${latitude},${longitude}${config.meteo}`
+        );
+        const data = await response.json();
+        
+        //Voir quel modif a faire (sur les noms des columns "2m" avec du regex)
+        return data
+    }catch (err) {
+        console.log("Can't find the weather", err)
+        return {};
+    }
+}
+
