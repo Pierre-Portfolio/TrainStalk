f9743c93546227aab5407ce98d22cc829847c8b6
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-03-23 23:38:03 +0100

    create first onthology

--- Onthologies/ScriptGeneration/main.py
+++ Onthologies/ScriptGeneration/main.py
@@ -1,18 +1,23 @@
-from rdflib import Graph, URIRef, Literal
-from rdflib.serializer import Serializer
 import json
+from rdflib import Graph, Literal, RDF, URIRef
+from rdflib.namespace import FOAF , XSD
 
-data = open('test.txt', 'r').readlines()
-str_data=""
-for i in data:
-    str_data = str_data+str(i)
+data_context = json.load(open('../Data/Gare/context.json', 'r'))
+data_gare = json.load(open('../Data/Gare/data.json', 'r'))
 
-data_context = json.load(open('context.json', 'r'))
-data_gare = json.load(open('liste-des-gares.json', 'r'))
-
-str_context = str(data_context)
-
-context = data_context["@context"]
-
-g = Graph().parse(str_data, format='application/rdf+owl')
-#print(g.serialize(format='json-ld', indent=4))
+def GenerateJsonLD():
+    data_context["itemListElement"].append(data_gare)
+    with open("../Data/Gare/JsonLD.json","w",encoding="utf-8") as file:
+        json.dump(data_context, file)
+    return data_context
+    
+def GenerateRDF():
+    str_data = GenerateJsonLD()
+    g = Graph()
+    g.parse(data=str_data, format='json-ld')
+    
+    rdf = ""
+    for s, p, o in g:
+        rdf += str(s) + " " + str(p) + " " + str(o) + " . "
+    with open("../Data/Gare/result.txt","w",encoding="utf-8") as file:
+        json.dump(rdf, file)
