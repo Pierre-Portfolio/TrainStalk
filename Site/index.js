bf0a56e18ccea7167949e7ba4ed27783521de96a
Elie OBADIA <elie.obadia@edu.devinci.fr>
2022-03-24 11:42:33 +0100

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express()
+const PORT = 8092;
+
+module.exports = app;
 
 app.use(express.static('public'));
+app.use(cors());
+app.use(helmet());
 
-app.listen(80,
-    () => console.log("server listening on port 80")
-);
+app.options('*', cors());
+
+app.get('/trajet/gare/search', (request, response) => {/*recup trajet gare arrive et depart*/});
+
+app.get('/trajet/:num_gare', (request, response) => {/*recup trajet nb train*/});
+
+app.get('/', (request, response) => {/*recup gare*/});
+
+app.listen(PORT);
+
+console.log(`📡 Running on port ${PORT}`);
