ebfa0bef9773342ea4e3930806987b674802a4c1
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-03-24 12:06:56 +0100

    go fixer l'image a droite mais je te laisse la choisir

--- Site/src/GenerateRdf.js
+++ Site/src/GenerateRdf.js
@@ -13,7 +13,7 @@ const GenerateRdfDynamic = async (data_context, data_contains) => {
 
 // a faire pour les 2 lien dynamique donc recup leur json respectif
 const rdfGare = await GenerateRdfDynamic('./src/Onthologies/Data/Gare/context.json','./src/Onthologies/Data/Gare/data.json')
-
+console.log(rdfGare)
 //pour les requetes voir ce tuto
 //https://zazuko.com/get-started/developers/
 //https://www.npmjs.com/package/sparql-http-client
