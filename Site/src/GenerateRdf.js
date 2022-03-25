c40e6369a3821eb308357dfa31ea69b8281ddb28
Flaye <45353885+Flaye@users.noreply.github.com>
2022-03-25 16:26:53 +0100

    :mega: correction error in generaterdf

--- Site/src/GenerateRdf.js
+++ Site/src/GenerateRdf.js
@@ -24,10 +24,7 @@ var prefix = "PREFIX owl:http://www.w3.org/2002/07/owl#\n" +
     "PREFIX rdf:http://www.w3.org/1999/02/22-rdf-syntax-ns#\n" +
     "PREFIX xml:http://www.w3.org/XML/1998/namespace\n" +
     "PREFIX xsd:http://www.w3.org/2001/XMLSchema#\n" +
-    "PREFIX rdfshttp://www.w3.org/2000/01/rdf-schema#"
-
-
-var q =
+    "PREFIX rdfshttp://www.w3.org/2000/01/rdf-schema#";
 
 var store = new Store({name:"test", overwrite:true}, function (err,store){
     store.load('text/n3', rdfGare, function (s,d){
