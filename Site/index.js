f390a01e7d8193115689dbad38c98c46553d6c8d
Kodamas <superjason95@gmail.com>
2022-03-26 04:56:04 +0100

    Update

---
+++ Site/index.js
@@ -0,0 +1,239 @@
+const express = require("express");
+const helmet = require('helmet');
+const cors = require('cors');
+const rdfstore = require('rdfstore');
+const {
+    GenerateRdfDynamic,
+    GenerateRdfDynamicWithoutUrl
+} = require('./src/GenerateRdf.js');
+
+const app = express()
+const PORT = 8092;
+const fs = require('fs')
+
+app.use(require('body-parser').json());
+app.use(express.static('public'));
+app.use(cors());
+app.use(helmet());
+
+app.options('*', cors());
+
+/*recup trajet gare arrive et depart*/
+app.post('/meteo', async (request, response) => {
+    console.log("Debut /meteo");
+    const rdfWeather = await GenerateRdfDynamicWithoutUrl("./src/Onthologies/Data/Weather/context.json", request.body);
+    console.log(rdfWeather)
+})
+
+/*recup trajet gare arrive et depart*/
+app.post('/trajet/gare/search', async (request, response) => {
+    console.log("Debut /trajet/gare/search");
+    let gare_dep = request.body.garedep;
+    let gare_arr = request.body.garearr;
+    let resp = {};
+    try {
+        const rdfGare = await GenerateRdfDynamic("./src/Onthologies/Data/Gare/context.json", "./src/Onthologies/Data/Gare/data.json");
+        let store = new rdfstore.Store((err, store) => {
+            store.load('text/n3', rdfGare, (s, d) => {
+                store.execute(query_uicFromGare(gare_dep), (err, res) => {
+                    if (res.length !== 0) {
+                        resp['garedep'] = res[0].UIC.value;
+                        store.execute(query_uicFromGare(gare_arr), (err, res) => {
+                            if (res.length !== 0) {
+                                resp['garearr'] = res[0].UIC.value;
+                                response.send({
+                                    "uic": resp,
+                                    "success": true
+                                })
+                            } else {
+                                store.execute(query_uicFromGare(gare_arr.toUpperCase()), (err, res) => {
+                                    if (res.length !== 0) {
+                                        resp['garearr'] = res[0].UIC.value;
+                                        response.send({
+                                            "uic": resp,
+                                            "success": true
+                                        })
+                                    } else {
+                                        response.send({
+                                            "success": false
+                                        })
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        store.execute(query_uicFromGare(gare_dep.toUpperCase()), (err, res) => {
+                            if (res.length !== 0) {
+                                resp['garedep'] = res[0].UIC.value;
+                                store.execute(query_uicFromGare(gare_arr), (err, res) => {
+                                    if (res.length !== 0) {
+                                        resp['garearr'] = res[0].UIC.value;
+                                        response.send({
+                                            "uic": resp,
+                                            "success": true
+                                        })
+                                    } else {
+                                        store.execute(query_uicFromGare(gare_arr.toUpperCase()), (err, res) => {
+                                            if (res.length !== 0) {
+                                                resp['garearr'] = res[0].UIC.value;
+                                                response.send({
+                                                    "uic": resp,
+                                                    "success": true
+                                                })
+                                            } else {
+                                                response.send({
+                                                    "success": false
+                                                })
+                                            }
+                                        });
+                                    }
+                                });
+                            } else {
+                                response.send({
+                                    "success": false
+                                })
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    } catch (e) {
+        console.log(e);
+        response.send({
+            "success": false
+        })
+    }
+});
+
+/* === Get journey of a train === */
+app.post('/trajet/id', async (request, response) => {
+    console.log("Debut /trajet/id");
+    let id = request.body.id;
+    try {
+        const rdfTrajet = await GenerateRdfDynamicWithoutUrl("./src/Onthologies/Data/Train/context.json", request.body.val);
+        let store = new rdfstore.Store(function (err, store) {
+            store.load('text/n3', rdfTrajet, (s, d) => {
+                store.execute(ASK_getJourney(id), function (err, res) {
+                    if (res === true) {
+                        store.execute(getJourney(id), function (err, res) {
+                            if (res && res.length !== 0) {
+                                let val_res = res.map(x => {
+                                    return {
+                                        "station_name ": x.stop_name.value,
+                                        "arrival ": x.arrival.value,
+                                        "departure ": x.depart.value,
+                                        "lat": x.lat.value,
+                                        "long": x.long.value,
+                                        "size": x.size.value
+                                    }
+                                });
+                                response.send({
+                                    "values": val_res,
+                                    "success": true
+                                })
+                            } else {
+                                response.send({
+                                    "success": false
+                                })
+                            }
+                        });
+                    } else {
+                        response.send({
+                            "success": false
+                        })
+                    }
+                });
+            });
+        });
+    } catch (e) {
+        console.log(e);
+        response.send({
+            "success": false
+        })
+    }
+});
+
+/*== Get all station on start ==*/
+app.post('/gare', async (request, response) => {
+    try {
+        const rdfGare = fs.readFileSync('./src/Onthologies/Data/Gare/gare-data.nq').toString();
+        let store = new rdfstore.Store(function (err, store) {
+            store.load('text/n3', rdfGare, (s, d) => {
+                store.execute(query_allGareName, function (err, res) {
+                    let val_ret = res.map(x => x.name.value);
+                    if (val_ret.length !== 0) {
+                        response.send({
+                            "All_Gare": val_ret,
+                            "success": true
+                        })
+                    } else {
+                        response.send({
+                            "success": false
+                        })
+                    }
+                });
+            });
+        });
+    } catch (e) {
+        console.log(e);
+        response.send({
+            "success": false
+        })
+    }
+    console.log("Liste des gares : Done...");
+});
+
+app.get('/', (request, response) => {});
+
+app.listen(PORT);
+
+console.log(`📡 Running on port ${PORT}`);
+console.log(`Open it with http://localhost:${PORT}`)
+
+/* === Function === */
+
+function saveJson(textName, jsonData) {
+    fs.writeFile(textName, jsonData, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+/* === Query === */
+const query_allGareName = "SELECT ?name WHERE{?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7NomGare> ?name}"
+
+const query_uicFromGare = (gareName) => {
+    return `SELECT ?UIC WHERE` +
+        `{ {?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7NomGare> "${gareName}"} UNION` +
+        `{?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Ville> "${gareName}" }.` +
+        `?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7IDGare> ?UIC}`;
+}
+
+/* == Check if idtrain == value in RDF ==*/ //TrainName a changer
+const ASK_getJourney = (idtrain) => {
+    return `ASK {?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7train_id> "${idtrain}"}`
+}
+
+const getJourney = (idtrain) => {
+    return `SELECT ?arrival ?depart ?stop_name ?lat ?long ?size WHERE {
+?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7train_id> "9580".
+?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_times> ?y.
+?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Arrive> ?arrival.
+?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Depart> ?depart.
+?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_point> ?z.
+?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Name> ?stop_name.
+?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7coord> ?a.
+?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLatitude> ?lat.
+?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLongitude> ?long .
+?c <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Size> ?size
+}`
+}
+
+const get_coord = (gareName) => {
+    return `SELECT ?cood WHERE` +
+        `{ {?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7NomGare> "${gareName}"} UNION` +
+        `{?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Ville> "${gareName}"}.` +
+        `?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Coordinates> ?coord}`;
+}
--- Site/src/GenerateRdf.js
+++ Site/src/GenerateRdf.js
@@ -1,61 +1,68 @@
-import * as jsonld from 'jsonld';
-import * as fs from 'fs';
-import * as rdfstore from 'rdfstore'
+const jsonld = require('jsonld');
+const fs = require('fs');
+// const rdfstore = require('rdfstore');
 
-export const GenerateRdfDynamicWithoutUrl = async (data_context, data_contains) => {
+const GenerateRdfDynamicWithoutUrl = async (data_context, data_contains) => {
     let jsoncontext = JSON.parse(fs.readFileSync(data_context, 'utf-8'))
     jsoncontext["itemListElement"].push(data_contains)
 
-    const rdf = await jsonld.default.toRDF(jsoncontext, { format: 'application/n-quads' });
+    const rdf = await jsonld.toRDF(jsoncontext, {
+        format: 'application/n-quads'
+    });
     return rdf
 };
 
-export const GenerateRdfDynamic = async (data_context, data_contains) => {
+exports.GenerateRdfDynamicWithoutUrl = GenerateRdfDynamicWithoutUrl;
+
+const GenerateRdfDynamic = async (data_context, data_contains) => {
     let jsoncontext = JSON.parse(fs.readFileSync(data_context, 'utf-8'))
     const jsoncontains = JSON.parse(fs.readFileSync(data_contains, 'utf-8'))
     jsoncontext["itemListElement"].push(jsoncontains)
 
-    const rdf = await jsonld.default.toRDF(jsoncontext, { format: 'application/n-quads' });
+    const rdf = await jsonld.toRDF(jsoncontext, {
+        format: 'application/n-quads'
+    });
     return rdf
 };
+
+exports.GenerateRdfDynamic = GenerateRdfDynamic;
 
 // a faire pour les 2 lien dynamique donc recup leur json respectif
 
 
 //const rdfGare = await GenerateRdfDynamic('./src/Onthologies/Data/Gare/context.json','./src/Onthologies/Data/Gare/data.json')
 //console.log(rdfGare)
-const rdfTrajet = await GenerateRdfDynamic('./src/Onthologies/Data/Train/context.json','./src/Onthologies/Data/Train/trajet-data.json')
-console.log(rdfTrajet);
+// const rdfTrajet = await GenerateRdfDynamic('./src/Onthologies/Data/Train/context.json','./src/Onthologies/Data/Train/trajet-data.json')
 
-var prefix = "PREFIX owl:http://www.w3.org/2002/07/owl#\n" +
-    "PREFIX rdf:http://www.w3.org/1999/02/22-rdf-syntax-ns#\n" +
-    "PREFIX xml:http://www.w3.org/XML/1998/namespace\n" +
-    "PREFIX xsd:http://www.w3.org/2001/XMLSchema#\n" +
-    "PREFIX rdfs:http://www.w3.org/2000/01/rdf-schema#";
+// var prefix = "PREFIX owl:http://www.w3.org/2002/07/owl#\n" +
+//     "PREFIX rdf:http://www.w3.org/1999/02/22-rdf-syntax-ns#\n" +
+//     "PREFIX xml:http://www.w3.org/XML/1998/namespace\n" +
+//     "PREFIX xsd:http://www.w3.org/2001/XMLSchema#\n" +
+//     "PREFIX rdfs:http://www.w3.org/2000/01/rdf-schema#";
 
 //let q = `SELECT ?stop_name ?arrival WHERE {{?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Train_id> "9580".?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_times> ?y. ?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_point> ?z. ?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Station_Name> ?stop_name} UNION {?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Train_id> "9580". ?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_times> ?y. ?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Arrive> ?arrival }}`
 
-let q = `SELECT ?arrival ?depart ?stop_name ?lat ?long WHERE {
-?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7train_id> "9580".
-?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_times> ?y.
-?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Arrive> ?arrival.
-?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Depart> ?depart.
-?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_point> ?z.
-?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Name> ?stop_name.
-?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7coord> ?a.
-?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLatitude> ?lat.
-?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLongitude> ?long
-}`
+// let q = `SELECT ?arrival ?depart ?stop_name ?lat ?long WHERE {
+// ?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7train_id> "9580".
+// ?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_times> ?y.
+// ?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Arrive> ?arrival.
+// ?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Depart> ?depart.
+// ?y <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7stop_point> ?z.
+// ?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7Name> ?stop_name.
+// ?z <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7coord> ?a.
+// ?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLatitude> ?lat.
+// ?a <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7TrainLongitude> ?long
+// }`
 
 /*
 let q = `ASK {?x <http://www.semanticweb.org/tompa/ontologies/2022/2/untitled-ontology-7train_id> "9580"}`
 */
-var store = new Store({name:"test", overwrite:true}, function (err,store){
-    store.load('text/n3', rdfTrajet, function (s,d){
-        store.execute(q, function (success, results){
-            console.log(results)
-        });
-    });
-});
+// var store = new Store({name:"test", overwrite:true}, function (err,store){
+//     store.load('text/n3', rdfTrajet, function (s,d){
+//         store.execute(q, function (success, results){
+//             console.log(results)
+//         });
+//     });
+// });
 
 //fs.writeFileSync('./src/Onthologies/Data/Gare/gare-data.nq', rdfGare)
